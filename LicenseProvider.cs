07ef78a7935901bffeba5f5b16a8aa241afe16f4
danielmarbach <daniel.marbach@openplace.net>
2019-02-25 09:31:32 +0100

    Basic nuget package code

---
+++ LicenseProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using NServiceBus;
+
+namespace SampleLicenseNuget
+{
+    public static class LicenseProvider
+    {
+        public static void ApplyLicense(this EndpointConfiguration configuration)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "SampleLicenseNuget.License.xml";
+
+            using (var stream = assembly.GetManifestResourceStream(resourceName))
+            using (var reader = new StreamReader(stream))
+            {
+                configuration.License(reader.ReadToEnd());
+            }
+        }
+    }
+}
